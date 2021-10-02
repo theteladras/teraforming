4679d62c94cdee7768a4446e86736d0948e6aaa9
Sanel Hadzini <sanel.hadzini@yahoo.com>
2021-10-02 18:28:39 +0200

    added load balancer

--- teraforming-prod.tf
+++ teraforming-prod.tf
@@ -1,66 +1,95 @@
 provider "aws" {
-  profile = "default"
-  region = "us-west-2"
+  profile                 = "default"
+  region                  = "us-west-2"
   shared_credentials_file = "./credentials"
 }
 
 resource "aws_s3_bucket" "teraforming_bucket" {
   bucket = "teraforming-bucket-20210929"
-  acl = "private"
+  acl    = "private"
 }
 
 resource "aws_default_vpc" "default" {}
 
+resource "aws_default_subnet" "subnet_az1" {
+  availability_zone = "us-west-2a"
+  tags              = {
+    "teraforming": "true"
+  }
+}
+
+resource "aws_default_subnet" "subnet_az2" {
+  availability_zone = "us-west-2b"
+  tags              = {
+    "teraforming": "true"
+  }
+}
+
 resource "aws_security_group" "sec_teraforming_prod" {
-  name = "sec_teraforming_prod"
+  name        = "sec_teraforming_prod"
   description = "allow inbound http and https and all for outbound"
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
+  tags          = {
     "teraforming": "true"
   }
 }
 
 resource "aws_instance" "instance_teraforming_prod" {
-  count = 2
-
-  ami = "ami-0ed34781dc2ec3964"
-  instance_type = "t2.nano"
+  count                  = 2
+  ami                    = "ami-0ed34781dc2ec3964"
+  instance_type          = "t2.nano"
   vpc_security_group_ids = [
     aws_security_group.sec_teraforming_prod.id
   ]
-
-  tags = {
+  tags                   = {
     "teraforming": "true"
   }
 }
 
 resource "aws_eip_association" "eip_associate_teraforming_prod" {
-  instance_id = aws_instance.instance_teraforming_prod.0.id
+  instance_id   = aws_instance.instance_teraforming_prod.0.id
   allocation_id = aws_eip.eip_teraforming_prod.id
 }
 
 resource "aws_eip" "eip_teraforming_prod" {
+  tags = {
+    "teraforming": "true"
+  }
+}
+
+resource "aws_elb" "loadbalancer_teraforming" {
+  name            = "teraforming"
+  instances       = aws_instance.instance_teraforming_prod.*.id
+  subnets         = [aws_default_subnet.subnet_az1.id, aws_default_subnet.subnet_az2.id]
+  security_groups = [aws_security_group.sec_teraforming_prod.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
   tags = {
     "teraforming": "true"
   }
