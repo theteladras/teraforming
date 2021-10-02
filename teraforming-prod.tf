c1bd7808333f866ab2a7030c15887ca46f8fa4bd
Sanel Hadzini <sanel.hadzini@gmail.com>
2021-10-02 14:24:16 +0200

    added security group

--- teraforming-prod.tf
+++ teraforming-prod.tf
@@ -10,3 +10,32 @@ resource "aws_s3_bucket" "teraforming_bucket" {
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "sec-teraforming-prod" {
+  name = "sec-teraforming-prod"
+  description = "allow inbound http and https and all for outbound"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "teraforming": "true"
+  }
+}
